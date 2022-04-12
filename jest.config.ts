02a8a76f129b996834b01112c03658e64ad8e75d
renanferreira <renan.sant@ibm.com>
2022-04-11 21:55:25 -0300

    correct alias and jest config to work

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,8 @@ const config: Config.InitialOptions = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
+    '^@pages(.*)$': '<rootDir>/src/pages/$1',
+    '^@components(.*)$': '<rootDir>/src/components/$1',
   },
 };
 
