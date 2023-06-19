3a76d655dfa67d41f81d94879ed0a14c0fd2e0f9
reenanfs <renan.sant@ibm.com>
2023-06-18 21:18:29 -0300

    update to when there is no env file

--- config/env.js
+++ config/env.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const env = dotenv.config().parsed;
-exports.envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
+const envKeys = {};
+
+if (env) {
+  envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+}
+
+exports.envKeys = envKeys;
